39897f8dd9e74f422f6d283fb78f221c5cbaa20e
Lakshya kalra <lakshyakalra1403@gmail.com>
2024-06-24 00:38:23 +0530

    third commit

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 var loader=document.querySelector("#loader");
-
+var menu=document.getElementById("menu");
 setTimeout(function(){
     loader.style.top="-100%";
 },3800)
+
+
+function menuslider(){
+    // menu.style.transform = "translateY(1%)";
+    menu.classList.toggle("toggler");
+    menu.classList.toggle("up");
+}
+
+function toportfolio(){
+    window.open("https://lakshya-klr.github.io/My-Portfolio-responsive/");
+}
+function mailme(){
+    window.location.href="mailto:lakshyakalra1403@gmail.com"
+}
