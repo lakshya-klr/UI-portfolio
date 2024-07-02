8b54b420db337cf30e3e9ad49231cd7057d22f82
Lakshya kalra <lakshyakalra1403@gmail.com>
2024-07-03 00:00:50 +0530

    adding cards of tech

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ function toportfolio(){
 function mailme(){
     window.location.href="mailto:lakshyakalra1403@gmail.com"
 }
+document.getElementById('frontcard').addEventListener('mouseover', function() {
+    document.getElementById('lastcard').style.transform = 'rotate(0deg)';
+    document.getElementById('lastcard').style.scale = '0.9';
+    document.getElementById('frontcard').style.color="white";
+    document.getElementById('frontcard').style.backgroundColor="#1e1e1e";
+    document.getElementById('middlecard').style.transform = 'rotate(0deg)';
+    document.getElementById('middlecard').style.scale = '0.9';
+    document.getElementById('middlecard').style.transform = 'translateX(0)';
+});
+
+document.getElementById('frontcard').addEventListener('mouseout', function() {
+    document.getElementById('lastcard').style.transform = 'rotate(0deg)';
+    document.getElementById('lastcard').style.transform = 'translateX(-116.5%)';
+    document.getElementById('lastcard').style.scale = '1';
+    document.getElementById('frontcard').style.backgroundColor="#1e1e1e";
+    document.getElementById('frontcard').style.backdropFilter="blur(50px)";
+    document.getElementById('frontcard').style.color="white";
+    document.getElementById('middlecard').style.transform = 'rotate(0deg)';
+    document.getElementById('middlecard').style.scale = '1';
+    document.getElementById('middlecard').style.transform = 'translateX(116.5%)';
+});
+
+
+
+document.getElementById('frontcard').addEventListener('click', function() {
+    document.getElementById('middlecard').style.scale = '1';
+    document.getElementById('lastcard').style.scale = '1';
+    // document.getElementById('lastcard').style.transform = 'translateX(-116.5%);';
+
+});
