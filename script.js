56cf44544eb2eade4ddd6b3311da5065808286f0
Lakshya kalra <lakshyakalra1403@gmail.com>
2024-07-05 23:35:41 +0530

    final full mobile view draft

--- script.js
+++ script.js
@@ -60,3 +60,15 @@ document.getElementById('frontcard').addEventListener('click', function() {
     // document.getElementById('lastcard').style.transform = 'translateX(-116.5%);';
 
 });
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+            if(entry.isIntersecting){
+                entry.target.classList.add('show');
+            }
+            else{
+                // entry.target.classList.remove('show');
+            }
+    });
+});
+const hiddenelements = document.querySelectorAll('.hidden');
+hiddenelements.forEach((el)=>observer.observe(el));
